d40754c5e88acfb7f72bc4280cc8e67437c52b64
yousufmachiyara-tgm <yousufmachiyara.tgm@gmail.com>
2025-02-10 11:42:51 +0500

    fixes

--- app/Models/PurPO_old.php
+++ app/Models/PurPO_old.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -38,4 +37,5 @@ class PurPO extends Model
     {
         return $this->belongsTo(ChartOfAccounts::class, 'vendor_name', 'id'); 
     }
+
 }
