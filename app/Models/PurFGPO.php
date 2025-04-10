544068f478255c7725adb53c7deb7326bce06714
yousufmachiyara-tgm <yousufmachiyara.tgm@gmail.com>
2025-04-10 15:54:15 +0500

    Update PurFGPO.php

--- app/Models/PurFGPO.php
+++ app/Models/PurFGPO.php
@@ -26,7 +26,7 @@ class PurFGPO extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Products::class, 'product_id');
     }
 
     public function details()
