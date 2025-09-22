140d5bb2d96c4d6f4039a89e99b531d3d18a29bc
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2025-09-22 15:31:50 +0500

    fixes

--- resources/views/purchasing/fgpo-billing/index.blade.php
+++ resources/views/purchasing/fgpo-billing/index.blade.php
@@ -63,7 +63,7 @@
                   </tr>
                 @empty
                   <tr>
-                    <td colspan="6" class="text-center">No bills found.</td>
+                    <td colspan="7" class="text-center">No bills found.</td>
                   </tr>
                 @endforelse
               </tbody>
